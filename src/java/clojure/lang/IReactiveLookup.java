ed716d86b449acd62ca4724a88cad75200a1b411
Aaron Craelius <aaroncraelius@gmail.com>
2014-09-27 16:53:05 -0400

    Added IReactiveLookup interface.

---
+++ src/java/clojure/lang/IReactiveLookup.java
@@ -0,0 +1,6 @@
+package clojure.lang;
+
+// Not sure if this interface is really a good idea without having full
+// collection change notifications
+public interface IReactiveLookup extends IReactiveRef, ILookup{
+}
