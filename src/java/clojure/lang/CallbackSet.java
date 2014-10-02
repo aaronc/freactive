cee69472928433ed779bac60a4ad0b74a7d4de23
Aaron Craelius <aaroncraelius@gmail.com>
2014-10-02 16:18:34 -0400

    Added implicit initial key and this parameters to the callbacks. Only have 0-2 arg invokeAll functions now.

--- src/java/clojure/lang/CallbackSet.java
+++ src/java/clojure/lang/CallbackSet.java
@@ -5,6 +5,12 @@ import java.util.Map;
 public class CallbackSet {
     private Atom callbacks = new Atom(PersistentHashMap.EMPTY);
 
+    public CallbackSet(Object thisRef) {
+        this.thisRef = thisRef;
+    }
+
+    private final Object thisRef;
+
     private final IFn assocFn = new AFn() {
             @Override
             public Object invoke(Object cur, Object key, Object callback) {
@@ -39,7 +45,7 @@ public class CallbackSet {
                 Map.Entry e = (Map.Entry) s.first();
                 IFn fn = (IFn) e.getValue();
                 if(fn != null)
-                    fn.invoke();
+                    fn.invoke(e.getKey(), this);
             }
         }
     }
@@ -54,7 +60,7 @@ public class CallbackSet {
                 Map.Entry e = (Map.Entry) s.first();
                 IFn fn = (IFn) e.getValue();
                 if(fn != null)
-                    fn.invoke(arg1);
+                    fn.invoke(e.getKey(), thisRef, arg1);
             }
         }
     }
@@ -69,37 +75,7 @@ public class CallbackSet {
                 Map.Entry e = (Map.Entry) s.first();
                 IFn fn = (IFn) e.getValue();
                 if(fn != null)
-                    fn.invoke(arg1, arg2);
-            }
-        }
-    }
-
-    public void invokeAll(Object arg1, Object arg2, Object arg3)
-    {
-        IPersistentMap cbs = (IPersistentMap)callbacks.deref();
-        if(cbs.count() > 0)
-        {
-            for(ISeq s = cbs.seq(); s != null; s = s.next())
-            {
-                Map.Entry e = (Map.Entry) s.first();
-                IFn fn = (IFn) e.getValue();
-                if(fn != null)
-                    fn.invoke(arg1, arg2, arg3);
-            }
-        }
-    }
-
-    public void invokeAll(Object arg1, Object arg2, Object arg3, Object arg4)
-    {
-        IPersistentMap cbs = (IPersistentMap)callbacks.deref();
-        if(cbs.count() > 0)
-        {
-            for(ISeq s = cbs.seq(); s != null; s = s.next())
-            {
-                Map.Entry e = (Map.Entry) s.first();
-                IFn fn = (IFn) e.getValue();
-                if(fn != null)
-                    fn.invoke(arg1, arg2, arg3, arg4);
+                    fn.invoke(e.getKey(), thisRef, arg1, arg2);
             }
         }
     }
