70cafb28fb4a2aea34122f96a7954cc67e0c3792
Aaron Craelius <aaroncraelius@gmail.com>
2014-11-13 18:04:03 -0500

    Updated project.clj. Tagging as 0.1.0.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject freactive "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject freactive "0.1.0"
+  :description "Reactive atoms, expressions and cursors for Clojure"
+  :url "https://github.com/aaronc/freactive"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0-alpha3"]
